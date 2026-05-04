c6ce4ba99fab03b22403cf9ee003686d49204751
lexx-09 <leximaepacana375@gmail.com>
2026-05-04 14:14:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  const messageBox = document.getElementById("hiddenMessage");
+
+  if (messageBox) {
+    messageBox.innerText =
+      "Just so you know, you mean so much to me Jisil Mi. Looking forward sa imong debut 😜";
+  }
+}
